2c0e7ac7e264089c2aa90ba55e9b69ad16e53149
shortnd <ococncol@gmail.com>
2018-09-09 13:25:14 -0400

    end points for movies

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,8 +1,17 @@
 const MovieSchema = require('../models/Movie.js');
 
 module.exports.controller = (app) => {
+  // fetch all movies
+  app.get('/movies', (req, res) => {
+    MovieSchema.find({}, 'name description release_year genre', (error, movies) => {
+      if (error) { console.log(error); }
+      res.send({
+        movies,
+      });
+    });
+  });
   // Add A movie
-  app.post('/movies', (req, res) => {
+  app.post('/movies/add', (req, res) => {
     const newMovie = new MovieSchema({
       name: req.body.name,
       description: req.body.description,
