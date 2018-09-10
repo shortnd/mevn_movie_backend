b0f7fe8f005548b1f738b1c4901c4e1cb8bf98b3
shortnd <ococncol@gmail.com>
2018-09-09 21:57:34 -0400

    added a user login endpoint

--- controllers/Users.js
+++ controllers/Users.js
@@ -1,5 +1,13 @@
 const User = require('../models/User');
 
+const passportJWT = require('passport-jwt');
+const jwt = require('jsonwebtoken');
+
+const ExtractJWT = passportJWT.ExtractJwt;
+const jwtOptions = {};
+jwtOptions.jwtFromRequest = ExtractJWT.fromAuthHeaderWithScheme('jwt');
+jwtOptions.secretOrKey = 'thisisthesecretkey';
+
 module.exports.controller = (app) => {
   // register a user
   app.post('/users/register', (request, response) => {
@@ -20,4 +28,28 @@ module.exports.controller = (app) => {
       response.send({ user });
     });
   });
-}
+
+  // Login a user
+  app.post('/users/login', (request, response) => {
+    if (request.body.email && request.body.password) {
+      const email = request.body.email;
+      const password = request.body.password;
+      User.getUserByEmail(email, (error, user) => {
+        if (!user) {
+          response.status(404).json({ message: 'The user does not exist!' });
+        } else {
+          User.comparePassword(password, user.password, (error, isMatch) => {
+            if (error) throw error;
+            if (isMatch) {
+              const payload = { id: user.id };
+              const token = jwt.sign(payload, jwtOptions.secretOrKey);
+              response.json({ message: 'ok', token});
+            } else {
+              response.status(401).json({ message: 'The password is incorrect' });
+            }
+          });
+        }
+      });
+    }
+  });
+};
